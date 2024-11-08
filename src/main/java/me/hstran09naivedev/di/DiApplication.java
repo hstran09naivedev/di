b5abc98720ee1a0c2bb34048db379545228314e7
Son Tran <hstran09naivedev@gmail.com>
2024-11-08 19:29:52 +0700

    init project

---
+++ src/main/java/me/hstran09naivedev/di/DiApplication.java
@@ -0,0 +1,13 @@
+package me.hstran09naivedev.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiApplication.class, args);
+    }
+
+}
